7d2c6576570c6f586319918a252c79c98f91f3a5
nataliagonzalezd <88407058+nataliagonzalezd@users.noreply.github.com>
2021-08-19 18:24:47 -0300

    First commit E1
    here we go

--- js/products.js
+++ js/products.js
@@ -4,3 +4,40 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+const url= "https://japdevdep.github.io/ecommerce-api/product/all.json"
+async function ProductosJSON(url){
+let promise = await fetch(url);
+let array = await promise.json();
+
+let contenido= "";
+for (let index = 0; index < array.length; index++) {
+    let name=array[index].name;
+    let description= array[index].description;
+    let cost= array[index].cost;
+    let currency= array[index].currency;
+    let img= array[index].imgSrc;
+    let sc= array[index].soldCount;
+
+    contenido +=`
+    <a href="product-info.html" class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col-3">
+                <img src="` + img+ `" alt="` + description + `" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">`+ name +`</h4>
+                    <small class="text-muted">` + sc + ` artículos vendidos</small>
+                </div>
+                <p class="mb-1">` + description + `</p>
+                <p class="mb-1">`+"Costo:"+ " " + cost +" "+ currency + `</p>
+            </div>
+        </div>
+    </a>
+    `
+}
+document.getElementById("cat-list-container").innerHTML = contenido;
+}
+
+ ProductosJSON(url);
